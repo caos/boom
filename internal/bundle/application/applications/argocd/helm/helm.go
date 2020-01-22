4d68941edee8515a0e60bb33878d15697df8d88d
Stefan Benz <benz.stefan1993@gmail.com>
2020-01-22 11:21:34 +0100

    fix(argocd): updated argocd 1.3.6 > 1.4.0

--- internal/bundle/application/applications/argocd/helm/helm.go
+++ internal/bundle/application/applications/argocd/helm/helm.go
@@ -5,7 +5,7 @@ import "github.com/caos/boom/internal/templator/helm/chart"
 func GetChartInfo() *chart.Chart {
 	return &chart.Chart{
 		Name:    "argo-cd",
-		Version: "1.5.3",
+		Version: "1.6.4",
 		Index: &chart.Index{
 			Name: "argo",
 			URL:  "argoproj.github.io/argo-helm",
@@ -15,7 +15,7 @@ func GetChartInfo() *chart.Chart {
 
 func GetImageTags() map[string]string {
 	return map[string]string{
-		"argoproj/argocd":    "v1.3.6",
+		"argoproj/argocd":    "v1.4.0",
 		"quay.io/dexidp/dex": "v2.14.0",
 		"redis":              "5.0.3",
 	}
