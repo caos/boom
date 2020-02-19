d97d515c0bacdd1f7985714587fab069950ed4c8
Stefan Benz <stefan@caos.ch>
2020-02-19 18:11:07 +0100

    fix(helm): upgraded several helm charts

--- internal/bundle/application/applications/loki/helm/helm.go
+++ internal/bundle/application/applications/loki/helm/helm.go
@@ -5,7 +5,7 @@ import "github.com/caos/boom/internal/templator/helm/chart"
 func GetChartInfo() *chart.Chart {
 	return &chart.Chart{
 		Name:    "loki",
-		Version: "0.25.0",
+		Version: "0.25.1",
 		Index: &chart.Index{
 			Name: "loki",
 			URL:  "grafana.github.io/loki/charts",
