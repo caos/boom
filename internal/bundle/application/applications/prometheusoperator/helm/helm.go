93d9abc19d244ec953272424981cab130f51a7f2
Stefan Benz <stefan@caos.ch>
2020-03-05 11:22:39 +0100

    fix(chart): upgrade prometheus operator chart 8.10.1 > 8.10.2

--- internal/bundle/application/applications/prometheusoperator/helm/helm.go
+++ internal/bundle/application/applications/prometheusoperator/helm/helm.go
@@ -5,7 +5,7 @@ import "github.com/caos/boom/internal/templator/helm/chart"
 func GetChartInfo() *chart.Chart {
 	return &chart.Chart{
 		Name:    "prometheus-operator",
-		Version: "8.10.1",
+		Version: "8.10.2",
 	}
 }
 
@@ -16,7 +16,7 @@ func GetImageTags() map[string]string {
 		"jettech/kube-webhook-certgen":              "v1.0.0",
 		"quay.io/coreos/prometheus-operator":        "v0.36.0",
 		"quay.io/coreos/configmap-reload":           "v0.0.1",
-		"quay.io/coreos/prometheus-config-reloader": "v0.35.0",
+		"quay.io/coreos/prometheus-config-reloader": "v0.36.0",
 		"k8s.gcr.io/hyperkube":                      "v1.12.1",
 		"quay.io/prometheus/prometheus":             "v2.15.2",
 	}
